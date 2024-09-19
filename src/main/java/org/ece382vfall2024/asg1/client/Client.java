bc3de4883350097c42e8d37c291d47821cb6ad6f
Akshu <f9ynwa@outlook.com>
2024-09-19 03:21:37 -0500

    Initial

--- src/main/java/org/ece382vfall2024/asg1/client/Client.java
+++ src/main/java/org/ece382vfall2024/asg1/client/Client.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -11,27 +14,49 @@ public class Client {
 
     private static String IP = "localhost";
     private static int PORT = 1024;
-      Socket clientSocket;
+    Socket clientSocket;
 
      private PrintWriter out;
      private BufferedReader in;
 
+    DatagramPacket sendPacket;
+    byte[] sendData;
+
+    DatagramSocket clientUDPSocket;
+
     public static void main(String args[]) throws IOException {
         Client client = new Client();
         //printAllOption();
-        client.startConnection();
+        client.startTCPConnection();
+        client.startUDPConnection();
         Scanner input = new Scanner(System.in);
         String read;
         PrintWriter out;
         BufferedReader in;
         while(!(read = input.nextLine()).isEmpty())
         {
-            client.sendAndReadMessage(read);
+            if(read.equals("TCP")){
+                read = input.nextLine();
+
+                client.sendAndReadMessage(read);
+            } else if(read.equals("UDP")){
+                read = input.nextLine();
+                client.sendUDPMessage(read);
+            }
+
         }
-        client.cleanup();
+        client.tcpCleanup();
+        client.udpCleanup();
         }
 
-    private  void cleanup() {
+    private  void udpCleanup() {
+
+            if (clientUDPSocket != null) {
+                clientUDPSocket.close();
+            }
+
+    }
+    private  void tcpCleanup() {
         try {
             if (out != null) {
                 out.close();
@@ -53,10 +78,22 @@ public class Client {
             //return resp;
     }
 
-    private  void startConnection() throws IOException {
+    private void sendUDPMessage(String message) throws IOException{
+        sendData = message.getBytes();
+        sendPacket = new DatagramPacket(sendData, sendData.length, InetAddress.getByName("127.0.0.1"), 5001);
+        clientUDPSocket.send(sendPacket);
+    }
+
+    private  void startTCPConnection() throws IOException {
+
         clientSocket = new Socket(IP, PORT);
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    private  void startUDPConnection() throws IOException {
+        clientUDPSocket = new DatagramSocket();
+        clientUDPSocket.setSoTimeout(1000);
     }
 
 
--- src/main/java/org/ece382vfall2024/asg1/server/Server.java
+++ src/main/java/org/ece382vfall2024/asg1/server/Server.java
@@ -4,8 +4,9 @@ import org.ece382vfall2024.asg1.server.cache.CacheHandler;
 import org.ece382vfall2024.asg1.server.handler.ClientHandler;
 
 import java.io.FileNotFoundException;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.IOException;
+import java.net.*;
+import java.sql.Timestamp;
 
 public class Server {
 
@@ -13,8 +14,44 @@ public class Server {
 
     public static void main(String ars[]) throws FileNotFoundException {
 
+        new Thread(() -> tcpServer()).start();
+        new Thread(() -> udpServer()).start();
+    }
+
+    private static void udpServer()  {
+        DatagramSocket serverSocket = null;
+        try {
+             serverSocket = new DatagramSocket(Integer.parseInt("5001"));
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("Server Started. Listening for Clients on port 5001" + "...");
+        // Assume messages are not over 1024 bytes
+        byte[] receiveData = new byte[1024];
+        DatagramPacket receivePacket;
+        while (true) {
+            // Server waiting for clients message
+            receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            try {
+                serverSocket.receive(receivePacket);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            // Get the client's IP address and port
+            InetAddress IPAddress = receivePacket.getAddress();
+            int port = receivePacket.getPort();
+            // Convert Byte Data to String
+            String clientMessage = new String(receivePacket.getData(), 0, receivePacket.getLength());
+            // Print the message with log header
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+            System.out.println("[" + timestamp.toString() + " ,IP: " + IPAddress + " ,Port: " + port + "]  " + clientMessage);
+        }
+    }
+    private static void tcpServer() {
         CacheHandler cacheHandler = new CacheHandler();
         ServerSocket server = null;
+
         while(true) {
             try {
                 server = new ServerSocket(1024);
